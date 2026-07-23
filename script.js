17a46980f638a2b9657d2f72c423d2ce11899269
Jerald Bucud <jeraldbucud@gmail.com>
2026-07-24 08:44:28 +1000

    Use the Phase 1 homepage script only

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 'use strict';
 
 const coreScript = document.createElement('script');
-coreScript.src = 'script-core.js?v=portfolio-core-1';
-coreScript.onload = () => {
-  const academicScript = document.createElement('script');
-  academicScript.src = 'academic-home.js?v=cqu-formal-qualifications-1';
-  document.body.append(academicScript);
-};
+coreScript.src = 'script-core.js?v=phase-one-portfolio';
 document.body.append(coreScript);
